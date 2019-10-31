21de0f354447aff74218c70ea58c85730f90ed11
romainsn <r.corre@lyon.ort.asso.fr>
2019-10-31 16:29:46 +0100

    Ajout de code a script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-alert("ceci est un formulaire");
+// alert("ceci est un formulaire");
+
+// if("bouton" is onclick){
+//     console.log(validé)
+// }
+
+//function nomRecup() {
+  //  console.log("prénom")
+//}
+function nomRecup() {
+    let input = document.getElementById("look").value;
+    const regex = /\d/;
+    let found = input.match(regex);
+    console.log(found);
+    found ? console.log("trouvé") : console.log("pas trouvé");
+    // if (found) {
+    //     console.log("trouvé");
+    // } else {
+    //     console.log("pas trouvé");
+    // }
+    // console.log(found);
+    
+    // .value.toUpperCase();
+    // let div =  document.createElement("div");
+    // div.setAttribute('class', 'test');
+    
+    // document.body.appendChild(div);
+    // div.innerHTML = input;
+}
+
+
+//console.log("test");
+
